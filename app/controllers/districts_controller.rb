5085b66729d4e1ee42a7f0e154bc8a23ef9f634e
Zushisan <74838@supinfo.com>
2018-03-11 21:27:16 +0000

    changed neigh and dist controller to handle react data load

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,7 +1,12 @@
 class DistrictsController < ApplicationController
   def index
-    @districts = District.all
-    render json: {data: @districts}
+    if params[:neighbourhood_id]
+      @districts = District.where(neighbourhood_id: params[:neighbourhood_id])
+      render json: {data: @districts}
+    else
+      @districts = District.all
+      render json: {data:  @districts}
+    end
   end
 
   def show
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,12 @@
 class LocationsController < ApplicationController
   def index
-    @locations = Location.all
-    render json: {data: @locations}
+    if params[:district_id]
+      @locations = Location.where(district_id: params[:district_id])
+      render json: {data: @locations}
+    else
+      @locations = Location.all
+      render json: {data:  @locations}
+    end
   end
 
   def show
