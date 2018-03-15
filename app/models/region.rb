24403eedd4ed141c4678aaedbf5ee94a09cc6f2a
Zushisan <74838@supinfo.com>
2018-03-15 00:56:26 +0000

    lilttle fix for maps, region now return coord

--- app/models/region.rb
+++ app/models/region.rb
@@ -9,7 +9,11 @@ class Region < ApplicationRecord
       country_id: country_id,
       created_at: created_at,
       updated_at: updated_at,
-      cities_ids: cities.to_a.map{ |city| city.region_id}
+      cities_ids: cities.to_a.map{ |city| city.region_id},
+      least_lat: least_lat,
+      greatest_lat: greatest_lat,
+      least_lng: least_lng,
+      greatest_lng: greatest_lng
     }
   end
 end
