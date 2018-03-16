e43e2f0c444d6a0660af10439bc0bf8c253dbd5f
Zushisan <74838@supinfo.com>
2018-03-16 21:26:47 +0000

    logic of loading achievements on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,20 @@ class SessionsController < ApplicationController
 
   def create
 
-
     user = User.find_by_email(params[:email])
 
-    # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user
-      # logged in when they navigate around our website.
-      session[:user_id] = user.id
-      render json: {data: user}
+      @achievements = {
+        locations: [],
+        neighbourhoods: [],
+        cities: [],
+        regions: [],
+        countries: []
+      }
+      # session[:user_id] = user.id
+      find_achievements(user)
+
+      render json: {data: [user, @achievements]}
 
     else
     # If user's login doesn't work, send them back to the login form.
@@ -27,8 +32,124 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
 
   end
+
   private
     def user_params
       params.require(:user).permit(:email, :password)
     end
+
+    def find_achievements(user)
+      puts "finding achievements for #{user}"
+      achievements_locations = find_achievements_locations(user)
+      if achievements_locations
+        achievements_neigh = find_achievements_neigh(achievements_locations)
+
+      end
+    end
+
+    def find_achievements_locations(user)
+      # First, find all of the user locations
+      locations = user.locations.find_all()
+      if locations
+        puts "found locations: #{locations}"
+        locations
+      else
+        false
+      end 
+    end
+
+    def find_achievements_neigh(locations)
+      
+      locations_by_neigh = {}
+
+      # We organize our locations by neigh id keys     
+      locations.each do |location|
+        @achievements[:locations] << location #achievements is returned to the React client
+        if !locations_by_neigh[location.neighbourhood_id]
+          locations_by_neigh[location.neighbourhood_id] = []
+        end
+        locations_by_neigh[location.neighbourhood_id] << location
+      end
+      
+      # Now we have our locations arrays ordered by neighbourhood ids as keys
+      # We are gonna compare our locations arrays to the size of our neigh/locations join
+      neigh_list = locations.map do |location|        
+        neigh_to_count = location.neighbourhood
+
+        # How much locations does that neigh have ?
+        neigh_size = neigh_to_count.locations.count()
+        if neigh_size == locations_by_neigh[location.neighbourhood_id].length
+          # You get the Neigh achievements !!!!
+          puts "You get the #{neigh_to_count} achievements !!!!"
+
+          # Now we want to see you get the City achievements
+          find_achievements_cities(neigh_to_count)
+        else
+          false     
+        end
+      end
+    end
+
+    def find_achievements_cities(neighbourhoods)
+      neigh_by_city = {}
+
+      # We organize our neighbourhoods by neigh id keys     
+      neighbourhoods.each do |neighbourhood|
+        @achievements[:neighbourhoods] << neighbourhood #achievements is returned to the React client
+        if !neigh_by_city[neighbourhood.city_id]
+          neigh_by_city[neighbourhood.city_id] = []
+        end
+        neigh_by_city[neighbourhood.city_id] << neighbourhood
+      end
+      
+      # Now we have our neighbourhoods arrays ordered by neighbourhood ids as keys
+      # We are gonna compare our neighbourhoods arrays to the size of our neigh/neighbourhoods join
+      cities_list = neighbourhoods.map do |neighbourhood|        
+        city_to_count = neighbourhood.city
+
+        # How much neighbourhoods does that neigh have ?
+        city_size = city_to_count.neighbourhoods.count()
+        if city_size == neigh_by_city[neighbourhood.city_id].length
+          # You get the Neigh achievements !!!!
+          puts "You get the #{city_to_count} achievements !!!!"
+
+          # Now we want to see you get the City achievements
+          find_achievements_regions(city_to_count)
+        else
+          false     
+        end
+      end
+
+    end
+
+    def find_achievements_regions(cities)
+      cities_by_region = {}
+
+      # We organize our cities by neigh id keys     
+      cities.each do |city|
+        @achievements[:cities] << city #achievements is returned to the React client
+        if !cities_by_region[city.region_id]
+          cities_by_region[city.region_id] = []
+        end
+        cities_by_region[city.region_id] << city
+      end
+      
+      # Now we have our cities arrays ordered by city ids as keys
+      # We are gonna compare our cities arrays to the size of our neigh/cities join
+      regions_list = cities.map do |city|        
+        region_to_count = city.region
+
+        # How much cities does that neigh have ?
+        region_size = region_to_count.cities.count()
+        if region_size == cities_by_region[city.region_id].length
+          # You get the Neigh achievements !!!!
+          puts "You get the #{region_to_count} achievements !!!!"
+
+        else
+          false     
+        end
+      end
+
+    end
+
 end
