de28aff1a2753ab56e2c76cd9f1167ca1ef533bb
Zushisan <74838@supinfo.com>
2018-03-16 01:03:54 +0000

    minor change to neigh model return

--- app/models/neighbourhood.rb
+++ app/models/neighbourhood.rb
@@ -9,7 +9,11 @@ class Neighbourhood < ApplicationRecord
       city_id: city_id,
       created_at: created_at,
       updated_at: updated_at,
-      locations_ids: locations.to_a.map{ |location| location.neighbourhood_id}
+      locations_ids: locations.to_a.map{ |location| location.neighbourhood_id},
+      least_lat: least_lat,
+      greatest_lat: greatest_lat,
+      least_lng: least_lng,
+      greatest_lng: greatest_lng
     }
   end
 end
