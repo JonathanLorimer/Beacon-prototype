7ab702fc2766aafc59ffc0771d669cdce536f085
XiaofengLighthouseLabs <gaga0110@163.com>
2018-03-20 17:37:18 +0000

    fix creat_at in diary and achievement in back

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,9 +1,14 @@
 class DiariesController < ApplicationController
 
-  def index
-    @user = User.find(params[:user_id])
-    @locations = @user.locations.find_all()
-    puts @locations
-    render json: {data: @locations}
-  end
+ def index
+   @user = User.find(params[:user_id])
+   @locations = @user.locations.find_all()
+   locations_visited_at = []
+
+   @user.locations.select('locations.*, user_locations.created_at').each do |location|
+     locations_visited_at << {name: location.name, visited_at: location.created_at}
+   end
+
+   render json: { data: [@locations, locations_visited_at] }
+ end
 end
